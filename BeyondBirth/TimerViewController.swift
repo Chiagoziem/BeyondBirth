8e87e8f1fe9b196568803d23a612686c9f93c332
Rachael Metzger <mme226@g.uky.edu>
2018-11-07 18:34:11 -0500

    update timerViewController

--- BeyondBirth/TimerViewController.swift
+++ BeyondBirth/TimerViewController.swift
@@ -1,17 +1,28 @@
-//
-//  TimerViewController.swift
-//  BeyondBirth
-//
-//  Created by Rachael Metzger on 11/1/18.
-//  Copyright © 2018 Rachael Metzger. All rights reserved.
-//
+// Rachael Metzger
+// Beyond Birth Senior Design
+//  ViewController.swift
+//  Time
+// ---------------------------------------------
+// the code below runs a simple stopwatch
+// it starts at seconds, and increments
+// untill stopped or reset.
+// there is no limit to the time, it will
+// run infinitly if needed
+// ---------------------------------------------
+// code comment word count: 81
 
 import UIKit
 
-class TimerViewController: UIViewController {
+class ViewController: UIViewController {
+    // variables
+    var hours = 0
+    var minutes = 0
+    var seconds = 0
+    //    var fractions = 0
+    var timer = Timer()
+    
     // play button
     var playButton: UIButton!
-    
     func startButton() {
         playButton = UIButton(type: .system)
         playButton.backgroundColor = UIColor.blue
@@ -21,6 +32,7 @@ class TimerViewController: UIViewController {
         playButton.bounds = CGRect(x: 300, y: 300, width: 250, height: 50)
         playButton.center = CGPoint(x: 200, y: 400)
         playButton.titleLabel?.font = UIFont.systemFont(ofSize: 30)
+        playButton.addTarget(self, action: #selector(start(_:)), for: .touchUpInside)
         view.addSubview(playButton)
         
     }
@@ -36,6 +48,7 @@ class TimerViewController: UIViewController {
         pauseButton.bounds = CGRect(x: 300, y: 300, width: 250, height: 50)
         pauseButton.center = CGPoint(x: 200, y: 500)
         pauseButton.titleLabel?.font = UIFont.systemFont(ofSize: 30)
+        pauseButton.addTarget(self, action: #selector(stop(_:)), for: .touchUpInside)
         view.addSubview(pauseButton)
     }
     
@@ -50,14 +63,16 @@ class TimerViewController: UIViewController {
         resetButton.bounds = CGRect(x: 300, y: 300, width: 250, height: 50)
         resetButton.center = CGPoint(x: 200, y: 600)
         resetButton.titleLabel?.font = UIFont.systemFont(ofSize: 30)
+        resetButton.addTarget(self, action: #selector(reset(_:)), for: .touchUpInside)
         view.addSubview(resetButton)
     }
     
     // Timer lable
+    // evenually, we can add a second label just for fractions and it will run smoother
     //@IBOutlet weak var TimerLable: UILabel!
     var TimerLable: UILabel = {
         let lb = UILabel()
-        lb.frame = CGRect(x: 300, y: 300, width: 150, height: 50)
+        lb.frame = CGRect(x: 300, y: 300, width: 350, height: 100)
         lb.textAlignment = .center
         lb.textColor = .black
         lb.font = UIFont.systemFont(ofSize: 50)
@@ -66,17 +81,15 @@ class TimerViewController: UIViewController {
         return lb
         
     }()
-    var time = 0
-    var timer = Timer()
     
     func timerLB(){
+        
         view.addSubview(TimerLable)
         action()
     }
     
     // calls all views
     func setUpViews(){
-        view.backgroundColor = .white
         startButton()
         stopButton()
         restartButton()
@@ -86,23 +99,42 @@ class TimerViewController: UIViewController {
     // action functions
     // start
     @IBAction func start(_ sender: UIButton){
-        timer = Timer.scheduledTimer(timeInterval: 1, target: self, selector: #selector(TimerViewController.action), userInfo: nil, repeats: true)
+        [timer.invalidate()] // this takes care of speeding up clock on multiple clicks on start
+        timer = Timer.scheduledTimer(timeInterval: 0.1, target: self, selector: #selector(ViewController.action), userInfo: nil, repeats: true)
     }
     // stop
-    @IBAction func stop(_ sender: AnyObject){
+    @IBAction func stop(_ sender: UIButton){
         timer.invalidate()
     }
     
     // reset
-    @IBAction func reset(_ sender: AnyObject){
+    @IBAction func reset(_ sender: UIButton){
         timer.invalidate()
-        time = 0;
-        TimerLable.text = ("0.0")
+        //fractions = 0
+        seconds = 0
+        minutes = 0
+        hours = 0
+        TimerLable.text = ("0:0:0")
     }
     
+    // runs count so we can track seonds, minutes, and hour
     @objc func action(){
-        time += 1
-        TimerLable.text = String(time)
+        //        fractions += 1
+        //        if fractions > 99 {
+        //            seconds += 1
+        //            fractions = 0
+        //        }
+        seconds += 1
+        if seconds == 60{
+            minutes += 1
+            seconds = 0
+        }
+        if minutes == 60{
+            hours += 1
+            minutes = 0
+        }
+        
+        TimerLable.text = "\(hours):\(minutes):\(seconds) "
     }
     
     // run
@@ -111,3 +143,5 @@ class TimerViewController: UIViewController {
         setUpViews()
     }
 }
+
+
