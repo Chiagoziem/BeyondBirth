ada4c27c7fc9f95efce187b0a1983c7383045163
Chiagoziem <chiofuani@gmail.com>
2018-11-07 19:53:34 -0500

    Create JournalController.swift

---
+++ JournalController.swift
@@ -0,0 +1,19 @@
+//
+//  JournalController.swift
+//  team7
+//
+//  Created by Chiagoziem on 11/7/18.
+//  Copyright © 2018 Admin. All rights reserved.
+//
+
+import UIKit
+
+class JournalController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+
+        // Do any additional setup after loading the view.
+    }
+
+}
